745ab0f088ced96a34793b6e62532a7726416eef
FAYIZ FAZ <91145806+Fayizfaz@users.noreply.github.com>
2025-03-15 04:23:16 +0530

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Livaperfumes-/', // Set this to your GitHub repo name
   plugins: [react()],
+  base: '/Livaperfumes-/', // Update this to match your repo name
 });
