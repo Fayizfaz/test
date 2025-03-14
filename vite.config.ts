ee98097f201fb030ce863df5170fd61793b5e0df
FAYIZ FAZ <91145806+Fayizfaz@users.noreply.github.com>
2025-03-15 02:57:14 +0530

    Configured for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/perfumestest/', // Replace 'your-repo-name' with your actual GitHub repository name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
