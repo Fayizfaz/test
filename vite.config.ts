b8c4c00564d1cf89f4bad5510555f4bb79ab083a
FAYIZ FAZ <91145806+Fayizfaz@users.noreply.github.com>
2025-03-15 12:35:01 +0530

    Fixed build issues

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: "/test/", // Change this to match your GitHub repository name
   plugins: [react()],
-  base: '/test/',  // Must match your repo name exactly
 });
