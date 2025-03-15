08de4dd4ed7c71f66cca061dac36eeab30aa87de
FAYIZ FAZ <91145806+Fayizfaz@users.noreply.github.com>
2025-03-15 05:46:01 +0530

    Configured for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Livaperfumes-.github.io/', // Update with your repo name
+  base: '/Livaperfumes-', // Update with your repo name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
