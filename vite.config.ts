11c4d02e0761e16e41b9f5f677042a099e080d27
FAYIZ FAZ <91145806+Fayizfaz@users.noreply.github.com>
2025-03-17 13:52:09 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/test/", // Change this to match your GitHub repository name
+  base: "test", // Change this to match your GitHub repository name
   plugins: [react()],
 });
